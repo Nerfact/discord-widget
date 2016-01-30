cd89ab34072542f111b5a4681187b38610da9ddc
Josh Schmille <josh.schmille@gmail.com>
2016-01-29 21:23:54 -0600

    iframe version mostly done

--- discord-iframe-server/discord-iframe-server.js
+++ discord-iframe-server/discord-iframe-server.js
@@ -14,6 +14,8 @@ app.get('/', function(req, res){
 
 app.get('/render', function(req, res){
   var treeTop = '5px';
+  var treeBottom = '70px';
+  var usersOnlineBottom = '50px';
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<title>Discord iframe Widget</title>');
   res.write('<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" type="text/css">');
@@ -23,23 +25,38 @@ app.get('/render', function(req, res){
   res.write('<script type="text/javascript">');
   res.write('discordWidget.init({');
   res.write('serverId: \'' + req.query.id + '\',');
-    //serverId: '51143782082543616',
   if (req.query.title){
     res.write('title: \'' + req.query.title + '\',');
     treeTop = '34px';
   }
-    //title: 'Discord Widget Title',
-    //join: false,
-    //alphabetical: false,
-    //theme: 'dark',
-    //hideChannels: ['Channel Name 1', 'Channel Name 2'],
-    //showAllUsers: true,
+  if (req.query.theme){
+    res.write('theme: \'' + req.query.theme + '\',');
+  }
+  if (req.query.join){
+    res.write('join: ' + req.query.join + ',');
+    if (req.query.join == 'true') {
+      treeBottom = '70px';
+      usersOnlineBottom = '50px';
+    } else {
+      treeBottom = '20px';
+      usersOnlineBottom = '5px';
+    }
+  }
+  if (req.query.abc){
+    res.write('alphabetical: ' + req.query.abc + ',');
+  }
+  if (req.query.showall){
+    res.write('showAllUsers: ' + req.query.showall + ',');
+  }
+  if (req.query.toggle){
+    res.write('allUsersDefaultState: ' + req.query.toggle + ',');
+  }
     //allUsersDefaultState: true
   res.write('});');
   res.write('discordWidget.render();');
   res.write('</script>');
   res.write('<div class="discord-widget" style="height:100%;margin:0"></div>');
-  res.write('<style>.discord-fade{display:none;}.discord-join{position: absolute;bottom: 15px;width: auto;left:15px;right:15px;}.discord-tree{position: absolute;left: 15px;right:0;top:'+treeTop+';bottom: 70px;overflow-y: auto;overflow-x: hidden;}.discord-users-online{position: absolute;bottom: 50px;right: 15px;}</style>');
+  res.write('<style>.discord-fade{display:none;}.discord-join{position: absolute;bottom: 15px;width: auto;left:15px;right:15px;}.discord-tree{position: absolute;left: 15px;right:0;top:'+treeTop+';bottom:'+treeBottom+';overflow-y: auto;overflow-x: hidden;}.discord-users-online{position: absolute;bottom: '+usersOnlineBottom+';right: 15px;}</style>');
   res.end();
 });
 
